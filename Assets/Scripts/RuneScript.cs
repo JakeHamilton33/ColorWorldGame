ef9a5b31407b58deb99f5017d96770e9c4ede902
8BitHypocrite <im8bithyp@gmail.com>
2023-02-25 08:35:15 -0600

    Switchable Platforms Started
    a :)

---
+++ Assets/Scripts/RuneScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneScript : MonoBehaviour
+{
+    public GameObject[] SwitchablePlatforms;
+
+    public void swap()
+    {
+        foreach(var i in SwitchablePlatforms)
+        {
+            i.GetComponent<SwitchablePlatform>().switchColors();
+        }
+    }
+}
