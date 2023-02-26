e72cb8eae8f5d268659049cecf68e0271f2dd3f6
8BitHypocrite <im8bithyp@gmail.com>
2023-02-26 05:18:50 -0600

    Audiomanager
    ok

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,13 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public Sound[] sounds;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        foreach(Sound s in sounds)
+        {
+            gameObject.AddComponent<AudioSource>();
+        }
     }
 
     // Update is called once per frame
