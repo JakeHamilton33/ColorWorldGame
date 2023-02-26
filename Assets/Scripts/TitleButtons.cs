bc7b786d909d8484b050dbd831494b19176fe2ca
8BitHypocrite <im8bithyp@gmail.com>
2023-02-26 01:33:05 -0600

    TitleScreen
    hype

--- Assets/Scripts/TitleButtons.cs
+++ Assets/Scripts/TitleButtons.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleButtons : MonoBehaviour
 {
+    public Image ImageRenderer;
+    public ParticleSystem particles;
+
     public void startGame()
     {
         SceneManager.LoadScene("Main Scene");
@@ -13,5 +17,25 @@ public class TitleButtons : MonoBehaviour
     public void quitGame()
     {
         Application.Quit();
+    }
+
+    public void startFadeIn()
+    {
+        StartCoroutine(FadeTo());
+    }
+
+    IEnumerator FadeTo()
+    {
+        float alphaValue = ImageRenderer.color.a;
+        Color tmp = ImageRenderer.color;
+
+        while (alphaValue < 1)
+        {
+            alphaValue += .08f;
+            tmp.a = alphaValue;
+            ImageRenderer.color = tmp;
+
+            yield return new WaitForSeconds(.05f);
+        }
     }
 }
